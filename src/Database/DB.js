0b6e892dbeadb272c5de2bc99356d6c5f9d320fb
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2024-05-23 20:16:18 +0200

    database fil

---
+++ src/Database/DB.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAmoaI_ssISCBHpYBNXntWZ5OGDsD_m51I",
+    authDomain: "boulevarden-odense.firebaseapp.com",
+    projectId: "boulevarden-odense",
+    storageBucket: "boulevarden-odense.appspot.com",
+    messagingSenderId: "660052345006",
+    appId: "1:660052345006:web:0de5fd9fdc2f49ed1f6d3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
