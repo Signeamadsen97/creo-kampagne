06412b74dabe6a871d2384092338ae4a227f5969
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2024-05-28 09:30:27 +0200

    css og tekst

--- src/components/omcreo/TextImageBackground.vue
+++ src/components/omcreo/TextImageBackground.vue
@@ -3,18 +3,17 @@
 <template>
   <div class="wrapper">
     <div>
-      <h2 class="h1_poppins ">Hero text</h2>
-      <h2 class="h1_poppins">Hero text</h2>
+      <h2 class="h1_poppins header1">Bag projektet</h2>
+      <h2 class="h1_poppins header2">Creo Arkitekter</h2>
     </div>
     <div class="grid">
       <div class="text">
-        <p class="body_poppins">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-          deserunt suscipit blanditiis nihil nostrum possimus voluptatem sit
-          exercitationem porro quibusdam quae velit, quis ullam aliquid
-          provident, unde itaque. Consequatur necessitatibus dicta laborum rem
-          corporis quo iusto! Ipsum, quos? Aliquam ullam corrupti pariatur
-          recusandae repellat dicta, saepe animi et? Quod, assumenda!
+        <p class="h3_poppins">
+          Creo har en helt særlig relation til Odense Universitetshospital. <br> <br>
+          Den 13 etagers sengebygning, er nemlig opført i samarbejde med Jørgen Stærmose. <br> <br>
+          Han er også manden bag Creo Arkitekter, da han i sin tid grundlagde tegnestuen. <br> <br>
+          Creo Arkitekters relation til OUH og erfaring indenfor sundhedssektoren gør dem perfekte til dette projekt, med
+          deres tidligere erfaring.
         </p>
       </div>
       <div class="img">
@@ -27,16 +26,26 @@
 <style scoped>
 .grid {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1.3fr;
 }
 
 .h1_poppins {
-  text-align: center;
   padding-bottom: 30px;
 }
 
-.body_poppins {
-  width: 600px;
+.header1 {
+  margin-left: 20%
+}
+
+.header2 {
+  margin-left: 40%
+}
+
+.h3_poppins {
+  width: 500px;
+  color: black;
+  margin-left: 10%;
+  line-height: 3rem;
 }
 
 .wrapper {
