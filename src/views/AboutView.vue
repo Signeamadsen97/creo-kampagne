cf0dd578cb2b1f0a28dee95bbfc98e40b4598477
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2024-06-05 13:49:24 +0200

    img rettelse

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,6 @@ import CreoIntro from "@/components/omcreo/CreoIntro.vue";
 import HvemErVi from "@/components/omcreo/HvemErVi.vue";
 import TextImageBackground from "@/components/omcreo/TextImageBackground.vue";
 import TextMedia from "@/components/TextMedia.vue";
-import overblikImage from "@/components/icon/overblik.jpg"
 
 import kristinaImage from "@/components/icon/kristina.png";
 import simonImage from "@/components/icon/simon.png";
@@ -38,7 +37,7 @@ const profiles = [
     <TextMedia :about-us="true" :vertical="true"
       grid-text-top="Som arkitekter er vores største opgave at skabe rammerne, som vores liv og drømme udfolder sig i. <br/> <br/>
       Vores fokus er på bæredygtighed og genavendelse hvor det er muligt. Vores bygninger skal fremme livet, derfor lytter vi og er nysgerrige"
-      :grid-image-src="overblikImage" />
+      grid-image-src="src/components/icon/overblik.jpg" />
     <section id="why_do">
       <TextImageBackground />
     </section>
